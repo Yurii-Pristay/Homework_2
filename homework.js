3e039596dc476ae08271d0bbf396e536c3a51cc7
NastiiaV <awerga13@gmail.com>
2021-02-28 19:16:13 +0200

    Handled global errors

--- homework.js
+++ homework.js
@@ -17,35 +17,52 @@ const dbArray = [
 const server = http.createServer((req, res) => {
   // req.method -- get method
   // req.url -- get url
+  try{ 
 
-  // d route
-  if (req.url === '/d' || req.url.startsWith('/d?')) {
-    const d = new DCrud(dbArray, req);
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
+    console.log(asdl); // try to call some error
 
-    if (req.method === 'GET') {
-      d.read(req, res);
-    } else if (req.method === 'POST') {
-      d.create(req, res);
-    } else if (req.method === 'PUT') {
-      d.update(req, res);
-    } else if (req.method === 'DELETE') {
-      d.delete(req, res);
+
+    // d route
+    if (req.url === '/d' || req.url.startsWith('/d?')) {
+      const d = new DCrud(dbArray, req);
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+
+      if (req.method === 'GET') {
+        d.read(req, res);
+      } else if (req.method === 'POST') {
+        d.create(req, res);
+      } else if (req.method === 'PUT') {
+        d.update(req, res);
+      } else if (req.method === 'DELETE') {
+        d.delete(req, res);
+      }
+    }
+
+    // e routing
+    if (req.url === '/e' || req.url.startsWith('/e?')) {
+      res.writeHead(200, { 'Content-Type': 'application/json', })
+      const e = new ECrud(dbArray, req)
+      e.routing(req,res)
     }
   }
-
-  // e routing
-  if (req.url === '/e' || req.url.startsWith('/e?')) {
-    res.writeHead(200, { 'Content-Type': 'application/json', })
-    const e = new ECrud(dbArray, req)
-    e.routing(req,res)
+  catch(e){
+    console.log(e);
+    res.writeHead(500, {
+      'Content-Type': 'application/json',
+    });
+    res.end("server`s malformed");
   }
   
+  
 })
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
+
+
